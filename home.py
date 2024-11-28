ffd865eb446d69270f3e723fa0f635567137828a
SajanVPrince <sajanvprince@gmail.com>
2024-11-28 09:15:12 +0530

    sqlite3

--- home.py
+++ home.py
@@ -1,6 +1,12 @@
 #to instal flask (pip install flask)
 
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import sqlite3
+con=sqlite3.connect('D:/Sajan/flask/data.db')
+try:
+    con.execute("create table details(name text,age int)")
+except:
+    pass
 
 app=Flask(__name__)  #creating and app
 
@@ -12,8 +18,15 @@ def home():
 def welcome():         #function does not need a request parameter
     return "Welcome to our home"  #return doesn't need a httpresponce
 
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
+    con=sqlite3.connect('D:/Sajan/flask/data.db')
+    if request.method=='POST':
+        name=request.form.get('name')
+        age=request.form.get('age')
+        # print(name,age)
+        con.execute("insert into details(name,age)values(?,?)",(name,age))
+        con.commit()
     return render_template('index.html')
 
 @app.route('/sec')
